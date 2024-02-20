1c2b3433c7c575e7e239855f4bc6cac72dee664e
sonsauit <sontran3042003@gmail.com>
2024-02-20 14:21:32 +0700

    test

--- src/demo_java.java
+++ src/demo_java.java
@@ -10,26 +10,19 @@ public class demo_java {
 //        float diem = sc.nextFloat();
 //        System.out.println(""+sv +" co diem "+diem);
 
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Nhap vao tax = ");
-//        int tax = sc.nextInt();
-//
-//        if (tax < 10){
-//            System.out.println("Khong dong thue");
-//        } else if (10 <= tax && tax <= 15 ) {
-//            System.out.println("Thue 10%");
-//        } else if (15 < tax && tax <= 30) {
-//            System.out.println("thue 20%");
-//        } else
-//            System.out.println("thue 50%");
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao tax = ");
+        int tax = sc.nextInt();
 
-            System.out.println("Moi ban chon so: ");
-            int choose = sc.nextInt();
-
-            switch (choose){
-                case 1:
-            }
+        if (tax < 10){
+            System.out.println("Khong dong thue");
+        } else if (10 <= tax && tax <= 15 ) {
+            System.out.println("Thue 10%");
+        } else if (15 < tax && tax <= 30) {
+            System.out.println("thue 20%");
+        } else
+            System.out.println("thue 50%");
 
     }
     }
