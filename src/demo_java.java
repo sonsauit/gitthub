1bbdecf3ff0578967ad0346b9ba00572ae1d53a7
sonsauit <sontran3042003@gmail.com>
2024-02-20 13:20:42 +0700

    test_git

---
+++ src/demo_java.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class demo_java {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Nhap vao ten sv: ");
+//        String sv = sc.nextLine();
+//        System.out.println("Co diem: ");
+//        float diem = sc.nextFloat();
+//        System.out.println(""+sv +" co diem "+diem);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhap vao tax = ");
+//        int tax = sc.nextInt();
+//
+//        if (tax < 10){
+//            System.out.println("Khong dong thue");
+//        } else if (10 <= tax && tax <= 15 ) {
+//            System.out.println("Thue 10%");
+//        } else if (15 < tax && tax <= 30) {
+//            System.out.println("thue 20%");
+//        } else
+//            System.out.println("thue 50%");
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("Moi ban chon so: ");
+            int choose = sc.nextInt();
+
+            switch (choose){
+                case 1:
+            }
+
+    }
+    }
+
